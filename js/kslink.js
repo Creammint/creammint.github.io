f399d0f23ffb26208f09fc9bd23e93bf31f25221
Creammint <tangshunzhong869@gmail.com>
2024-01-25 17:23:21 +0800

    Site updated: 2024-01-25 17:23:21

--- js/kslink.js
+++ js/kslink.js
@@ -1,7 +1,7 @@
 var leonus = {
     linkCom: e => {
         var t = document.querySelector("#veditor");
-        "bf" == e ? (t.value = "```yml\n", t.value += "- name: \n  link: \n  avatar: \n  descr: \n  siteshot: ", t.value += "\n```", t.setSelectionRange(15, 15)) : (t.value = "站点名称：\n站点地址: \n头像链接: \n站点描述: \n站点截图: ", t.setSelectionRange(5, 5)), t.focus()
+        "bf" == e ? (t.value = "```yml\n", t.value += "- name：\n  link：\n  avatar：\n  descr：", t.value += "\n```", t.setSelectionRange(15, 15)) : (t.value = "站点名称：\n站点地址：\n头像链接： \n站点描述： ", t.setSelectionRange(5, 5)), t.focus()
     },
     owoBig: () => {
         if (!document.getElementById("post-comment") || document.body.clientWidth < 768) return;
