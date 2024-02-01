64babdead437648bf583cafc7aa037d8c617c6d6
Creammint <tangshunzhong869@gmail.com>
2024-02-01 15:21:25 +0800

    Site updated: 2024-02-01 15:21:25

--- js/main.js
+++ js/main.js
@@ -122,7 +122,7 @@ document.addEventListener('DOMContentLoaded', function () {
           data: function () {
             this.$notify({
               title: "哎嘿！复制成功🍬",
-              message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+              message: "若要转载最好保留原文链接哦，给你一个大大的赞！！！",
               position: 'top-left',
               offset: 50,
               showClose: true,
