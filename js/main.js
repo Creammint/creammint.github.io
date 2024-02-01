c86edfd5db916c1b7965331d486d6898f6a448d6
Creammint <tangshunzhong869@gmail.com>
2024-02-01 17:37:25 +0800

    Site updated: 2024-02-01 17:37:24

--- js/main.js
+++ js/main.js
@@ -117,20 +117,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const highlightToolsFn = function (e) {
       const $target = e.target.classList
       if ($target.contains('expand')) highlightShrinkFn(this)
-      else if ($target.contains('copy-button'))
-        new Vue({
-          data: function () {
-            this.$notify({
-              title: "哎嘿！复制成功🍬",
-              message: "若要转载最好保留原文链接哦，给你一个大大的赞！！！",
-              position: 'top-left',
-              offset: 50,
-              showClose: true,
-              type: "success",
-              duration: 5000
-            });
-          }
-        })
+      else if ($target.contains('copy-button')) highlightShrinkFn(this)
+      new Vue({
+        data: function () {
+          this.$notify({
+            title: "哎嘿！复制成功🍬",
+            message: "若要转载最好保留原文链接哦，给你一个大大的赞！！！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "success",
+            duration: 5000
+          });
+        }
+      })
     }
 
     const expandCode = function () {
